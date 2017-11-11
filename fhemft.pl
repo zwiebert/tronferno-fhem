3671d3091176fe33bbbbfe257738c838f8cf6d47
Bert Winkelmann <bertw@online.de>
2017-11-11 21:01:10 +0100

    command line options

--- fhemft.pl
+++ fhemft.pl
@@ -16,18 +16,21 @@ my $opt_g = 0;
 my $opt_m = 0;
 my $opt_c = "";
 my $opt_scan_log = 0;
+my $opt_sd_verbose = -1;
 my $opt_send = 0;
 my $opt_help = 0;
 my $logfile = "";
-
+my $opt_n = "sduino";
 
 GetOptions (
+            "n=s"   => \$opt_n, 
 	    "a=s"   => \$opt_a,    
 	    "g=i"   => \$opt_g,    
 	    "m=i"   => \$opt_m,
 	    "c=s"  => \$opt_c,
 	    "scan"  => \$opt_scan_log,
-	    "f" => \$logfile,
+            "f=s" => \$logfile,
+            "v=i" => \$opt_sd_verbose, 
 	    "send"   => \$opt_send,
 	    "h"  => \$opt_help,    
 	    "help"  => \$opt_help,    
@@ -49,8 +52,11 @@ commands: --scan, --send, --help
    -m=N             member number 1 ... 7.  or 0 for send to all members
    -c=string        command string: up, down, stop, sun-down, sun-inst, set
 
+   -n=string        name of SIGNALduino in FHEM (default: sduino)
+
   --scan            scan the current FHEM logfile for received Fernotron commands
-    -f FILE         use this fhem log file insted of default 
+    -f FILE         use this fhem log file insted of default
+    -v=N            set verbose level of SIGNALduino (must be at least 4 to receive commands)
 "
 }
 
@@ -78,10 +84,17 @@ my $year = $lt[5] + 1900;
 $logfile = "/opt/fhem/log/fhem-$year-$month.log";
 }
 
+if ($opt_sd_verbose ne -1) {
+    my $sd_cmd = "attr $opt_n verbose $opt_sd_verbose";
+    my $sys_cmd = "$tronferno::fhem_system '$sd_cmd'";
+    print "$sys_cmd\n";
+    system $sys_cmd;
+}
     
-$file=File::Tail->new(name=>$logfile, maxinterval=>1, adjustafter=>7, tail=>C0);
+$file=File::Tail->new(name=>$logfile, maxinterval=>1,  tail=> 0);
 
-print "Now press a button on your Fernotron controller device!\n";
+print "Now press a button on your Fernotron controller device! Press Ctrl-C if you are done\n";
+
     
 while (defined(my $line=$file->read)) {
     if ($line =~ /sduino\/msg READ:\s+\x02(M[US];.+)\x03/) {
@@ -113,7 +126,7 @@ if ($fsb != -1) {
     print "generated fernotron command: " . tronferno::fsb2string($fsb) . "\n";
 
     my $tx_data = tronferno::cmd2dString($fsb);
-    my $tx_cmd = "set sduino raw $tronferno::p_string$tx_data";
+    my $tx_cmd = "set $opt_n raw $tronferno::p_string$tx_data";
     my $sys_cmd = "$tronferno::fhem_system '$tx_cmd'";
 
     print "generated FHEM system command: $sys_cmd\n";
