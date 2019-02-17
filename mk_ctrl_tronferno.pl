7d805ecddbc72d3b39f4fa6113a845396e87f6e5
Bert Winkelmann <tf.zwiebert@online.de>
2019-02-17 23:43:58 +0100

    missing build scripts added

---
+++ mk_ctrl_tronferno.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use File::Basename;
+use POSIX qw(strftime);
+use strict;
+
+my @filenames = ('00_TronfernoMcu.pm', '10_Tronferno.pm');
+
+my $prefix = "FHEM";
+my $filename = "";
+foreach $filename (@filenames)
+{
+  my @statOutput = stat($prefix."/".$filename);
+  
+  if (scalar @statOutput != 13)
+  {
+    printf("error: stat has unexpected return value for ".$prefix."/".$filename."\n");
+    next;
+  }
+
+  my $mtime = $statOutput[9];
+  my $date = POSIX::strftime("%Y-%m-%d", localtime($mtime));
+  my $time = POSIX::strftime("%H:%M:%S", localtime($mtime));
+  my $filetime = $date."_".$time;
+
+  my $filesize = $statOutput[7];
+
+  printf("UPD ".$filetime." ".$filesize." ".$prefix."/".$filename."\n");
+}
+
+
+
+# Local Variables:
+# compile-command: "perl -cw -MO=Lint ./make_controlfile.pl"
+# End:
